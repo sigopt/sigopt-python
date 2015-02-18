b23ee4e95953bbff3931548437d9566784de2297
Patrick Hayes <pfhayes@gmail.com>
2015-02-18 12:56:53 -0800

    Set up for python package

---
+++ README.md
@@ -0,0 +1,6 @@
+== SigOpt Python API ==
+
+This is the SigOpt Python API client.
+Use this to natively call SigOpt API endpoints to create experiments and report data.
+
+Take a look in `examples` for example usage.
---
+++ setup.py
@@ -0,0 +1,21 @@
+# keep this in sync with requirements.txt
+install_requires=['requests>=2.0.1']
+
+setup(
+  name='pysigopt',
+  version='0.0.1',
+  description='SigOpt Python API Client',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['sigopt'],
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ]
+)
