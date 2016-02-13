c32191c8613aea6f57c1f21112db86ec5a69af00
Alexandra Leigh Johnson <alexandra@sigopt.com>
2016-02-12 17:28:05 -0800

    run command and not just executable

--- examples/README.md
+++ examples/README.md
@@ -3,16 +3,23 @@ For the complete API documentation, visit [https://sigopt.com/docs](https://sigo
 ## Python
 Check out our basic python example and implement your own function to evaluate. Dive deeper with our parallel example, which runs multiple optimization loops at the same time.
 ## Other Languages
-If you've got an executable file written in another language, you can use the example we've written where the `evaluate_metric` function run an executable file in a sub process. 
+If you've got an executable file written in another language, you can use the example we've written where the `evaluate_metric` function run an executable file in a sub process.
 
-This script will pass the suggested assignments as command line arguments, and will expect the script's output to be a float representing a fuction evaluated on these assignments. 
+This script will pass the suggested assignments as command line arguments, and will expect the script's output to be a float representing a fuction evaluated on these assignments.
 
-To start, you'll need to create an experiment with parameter names matching the command line arguments of your program. 
+To start, you'll need to create an experiment with parameter names matching the command line arguments of your program.
+
+Example Usage
 
 For example, if your filename is test and you have one double parameter with suggested value 11.05, this script will run
 ```
-./test --x=11.05
+python other_languages.py --command='./test --quiet' --experiment_id=2136 --client_token=$CLIENT_TOKEN
 ```
+The above command will run the following sub process
+```
+./test --quiet --x=SUGGESTION_FOR_X
+```
+
 Feel free to use, or modify for your own needs!
 
 
--- examples/other_languages.py
+++ examples/other_languages.py
@@ -6,15 +6,14 @@ import sys
 from sigopt.interface import Connection
 
 class SubProcessEvaluator(object):
-  def __init__(self, filename):
-    assert os.path.isfile(filename)
-    self.filename = filename
+  def __init__(self, command):
+    self.command = command
 
   # Take a suggestion from sigopt and evaluate your function
   # Sends command line arguments to your executable file with the same names as the
   # parameters of your experiment. Expected output is one line containing a float that
   # is your function evaluated at the suggested assignments.
-  # For example, if your filename is test and you have one double parameter with suggested
+  # For example, if your command is './test' and you have one double parameter with suggested
   # value 11.05, this script will run
   #         ./test --x=11.05
   def evaluate_metric(self, assignments):
@@ -23,7 +22,7 @@ class SubProcessEvaluator(object):
       for param_name, assignment
       in assignments.to_json().iteritems()
     ]
-    process = Popen(['./{}'.format(self.filename)] + arguments, stdout=PIPE, stderr=PIPE)
+    process = Popen(self.command.split() + arguments, stdout=PIPE, stderr=PIPE)
     (stdoutdata,stderrdata) = process.communicate()
     sys.stderr.write(stderrdata)
     return float(stdoutdata.strip())
@@ -31,9 +30,9 @@ class SubProcessEvaluator(object):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--filename', required=True, help="The name of the executable file containing the "
-    "function whose parameters you would like to optimize. Should accept parameters as command line argument and "
-    "output only the evaluated metric at the suggested point.")
+  parser.add_argument('--command', required=True, help="The command to run the function whose parameters you would "
+    "like to optimize. Should accept parameters as command line argument and output only the evaluated metric at the "
+    "suggested point.")
   parser.add_argument('--experiment_id', required=True, help="The parameters of this experiment should be the "
     "same type and name of the command line arguments to your executable file.")
   parser.add_argument('--client_token', required=True, help="Find your CLIENT_TOKEN at https://sigopt.com/user/profile")
@@ -42,7 +41,7 @@ if __name__ == '__main__':
   connection = Connection(client_token=the_args.client_token)
   experiment = connection.experiments(the_args.experiment_id).fetch()
   connection.experiments(the_args.experiment_id).suggestions().delete(state="open")
-  evaluator = SubProcessEvaluator(the_args.filename)
+  evaluator = SubProcessEvaluator(the_args.command)
 
   # In a loop: receive a suggestion, evaluate the metric, report an observation
   while True:
