a3b1b7f11743ffa840316054264adc19119cab1e
Taylor Jackle Spriggs <taylor@sigopt.com>
2021-03-19 09:15:02 -0600

    sigopt.get_run_id() (#185)
    * dont need git
    
    * fix import error
    
    * get run id
    
    Co-authored-by: Taylor Jackle Spriggs <taylor.spriggs@intel.com>

--- sigopt/__init__.py
+++ sigopt/__init__.py
@@ -1,19 +1,23 @@
 from .interface import Connection
 from .magics import SigOptMagics as _Magics
 from .runs import (
-  set_parameters,
+  RunFactory,
+  create_run,
   get_parameter,
   log_checkpoint,
   log_dataset,
+  log_failure,
+  log_image,
   log_metadata,
   log_metric,
   log_model,
-  log_failure,
-  log_image,
-  create_run,
+  set_parameters,
 )
 from .version import VERSION
 
 
 def load_ipython_extension(ipython):
   ipython.register_magics(_Magics)
+
+def get_run_id():
+  return RunFactory.get_global_run_context().run_id
