973b28c2f38d1405e71d0a6248cd08d4d24be21d
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-06-15 10:48:06 -0700

    experiment loop

--- sigopt/runs/experiment.py
+++ sigopt/runs/experiment.py
@@ -7,8 +7,21 @@ class ExperimentContext(object):
     self._experiment = experiment
 
   def create_run(self, name=None):
-    return LiveRunContext.create(
+    return LiveRunContext.create_from_experiment(
       connection=self._connection,
       run_name=name,
-      project_id=self._experiment.project,
+      experiment=self._experiment,
     )
+
+  def refresh(self):
+    self._experiment = self._connection.experiments(self._experiment.id).fetch()
+
+  def is_finished(self):
+    self.refresh()
+    if self._experiment.observation_budget is None:
+      return False
+    return self._experiment.progress.observation_budget_consumed >= self._experiment.observation_budget
+
+  def loop(self):
+    while not self.is_finished():
+      yield self.create_run()
