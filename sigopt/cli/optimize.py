98f988e594a1968a70d6c7abccb040114a82bf1e
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-06-18 14:02:06 -0700

    rm six

--- sigopt/cli/optimize.py
+++ sigopt/cli/optimize.py
@@ -4,7 +4,6 @@ import yaml
 from ..config import config
 from ..defaults import get_default_project
 from ..factory import SigOptFactory
-from ..vendored import six
 from .cli import cli
 from .validate import EXPERIMENT_KEY, validate_experiment_input, validate_sigopt_input
 from .utils import check_path, run_user_program, setup_cli
@@ -28,11 +27,11 @@ def get_and_validate_experiment_input(sigopt_input, filename):
 def optimize(entrypoint, entrypoint_args, sigopt_file):
   check_path(
     entrypoint,
-    six.u("Provided entrypoint '{entrypoint}' does not exist").format(entrypoint=entrypoint),
+    f"Provided entrypoint '{entrypoint}' does not exist",
   )
   check_path(
     sigopt_file,
-    six.u("The sigopt file '{sigopt_file}' is missing").format(sigopt_file=sigopt_file),
+    f"The sigopt file '{sigopt_file}' is missing",
   )
   sigopt_input = load_yaml(sigopt_file)
   experiment_input = get_and_validate_experiment_input(sigopt_input, sigopt_file)
