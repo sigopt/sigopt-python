6c9bd2f432751fc0e73587e5d5fea87864a3c98d
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-06-30 16:38:42 -0700

    remove sigopt service tests

--- test/orchestrate/sigopt/service_test.py
+++ test/orchestrate/sigopt/service_test.py
@@ -11,24 +11,9 @@ class TestSigOptService(object):
   def services(self):
     return Mock()
 
-  def test_with_api_token(self, services):
-    sigopt_service = SigOptService(services, api_token='foobar')
-    assert sigopt_service.conn is not None
-    assert sigopt_service.api_token == 'foobar'
-
-  def test_with_api_url(self, services):
-    sigopt_service = SigOptService(services, api_url='https://api-env.sigopt.com')
-    assert sigopt_service.conn is not None
-    assert sigopt_service.api_url == 'https://api-env.sigopt.com'
-
   def test_reads_from_environment(self, services):
     with patch.dict(os.environ, dict(SIGOPT_API_TOKEN='foobar', SIGOPT_API_URL='https://api-env.sigopt.com')):
       sigopt_service = SigOptService(services)
       assert sigopt_service.conn is not None
       assert sigopt_service.api_token == 'foobar'
       assert sigopt_service.api_url == 'https://api-env.sigopt.com'
-
-  def test_no_token_error(self, services):
-    with patch.dict(os.environ, dict(SIGOPT_API_TOKEN='')):
-      with pytest.raises(ValueError):
-        SigOptService(services).check_connection()
