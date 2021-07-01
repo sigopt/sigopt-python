2aa6c2a511486d8be2b22822c29a2fd1212e2291
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-06-30 17:45:13 -0700

    cached_property not supported before 3.8

--- sigopt/cli/commands/cluster/base.py
+++ sigopt/cli/commands/cluster/base.py
@@ -1,5 +1,3 @@
-from functools import cached_property
-
 import click
 
 from sigopt.orchestrate.controller import OrchestrateController
@@ -8,9 +6,13 @@ from ..base import sigopt_cli
 
 
 class Context:
-  @cached_property
+  _controller = None
+
+  @property
   def controller(self):
-    return OrchestrateController.create()
+    if self._controller is None:
+      self._controller = OrchestrateController.create()
+    return self._controller
 
 @sigopt_cli.group("cluster")
 @click.pass_context
