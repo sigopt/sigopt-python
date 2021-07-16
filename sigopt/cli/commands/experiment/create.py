30bd336ca85bd5dd85c1e210e13c6aac5b5e8bdb
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-07-16 11:25:14 -0700

    help msg

--- sigopt/cli/commands/experiment/create.py
+++ sigopt/cli/commands/experiment/create.py
@@ -1,10 +1,29 @@
+from sigopt.logging import print_logger
+
 from ...arguments import experiment_file_option
 from ...utils import create_experiment_from_validated_data
 from .base import experiment_command
 
 
+def print_help(experiment):
+  msg = f'''
+You can now start workers for this experiment with the following CLI command:
+> sigopt start-worker {experiment.id}
+
+Or use the python client library:
+
+  #/usr/bin/env python3
+  import sigopt
+  experiment = sigopt.get_experiment({experiment.id!r})
+  for run in experiment.loop():
+    with run:
+      ...
+'''
+  print_logger.info(msg)
+
 @experiment_command.command()
 @experiment_file_option
 def create(experiment_file):
   '''Create a SigOpt Experiment.'''
-  create_experiment_from_validated_data(experiment_file)
+  experiment = create_experiment_from_validated_data(experiment_file)
+  print_help(experiment)
