6360b369369caf0f8c43d631ffb9938732f99dc2
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-07-19 13:46:47 -0700

    remove code tracking cli test

--- test/cli/test_cli_config.py
+++ test/cli/test_cli_config.py
@@ -8,21 +8,17 @@ from sigopt.cli import cli
 
 class TestRunCli(object):
   @pytest.mark.parametrize('opt_into_log_collection', [False, True])
-  @pytest.mark.parametrize('opt_into_code_tracking', [False, True])
-  def test_config_command(self, opt_into_log_collection, opt_into_code_tracking):
+  def test_config_command(self, opt_into_log_collection):
     runner = CliRunner()
     log_collection_arg = '--enable-log-collection' if opt_into_log_collection else '--no-enable-log-collection'
-    code_tracking_arg = '--enable-code-tracking' if opt_into_code_tracking else '--no-enable-code-tracking'
     with mock.patch('sigopt.cli.commands.config._config.persist_configuration_options') as persist_configuration_options:
       result = runner.invoke(cli, [
         'config',
         '--api-token=some_test_token',
         log_collection_arg,
-        code_tracking_arg,
       ])
       persist_configuration_options.assert_called_once_with({
         'api_token': 'some_test_token',
-        'code_tracking_enabled': opt_into_code_tracking,
         'log_collection_enabled': opt_into_log_collection,
       })
     assert result.exit_code == 0
