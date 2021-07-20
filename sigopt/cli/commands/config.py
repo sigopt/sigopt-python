8cd90aceecff753553618e19995ab4dc0bb2f6a1
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-07-20 10:58:46 -0700

    cell tracking

--- sigopt/cli/commands/config.py
+++ sigopt/cli/commands/config.py
@@ -6,8 +6,13 @@ from .base import sigopt_cli
 
 LOG_COLLECTION_PROMPT = '''Log Collection
 \tThis will capture and upload the standard output and standard error of your
-\tRuns so that you can view them on the SigOpt dashboard.
+\tRuns from the CLI and notebook cells so that you can view them on the SigOpt dashboard.
 Enable log collection'''
+
+CELL_TRACKING_PROMPT = '''Notebook Cell Tracking
+\tThis will record and upload the content of your notebook cells so that you can view them
+\ton the SigOpt dashboard.
+Enable cell tracking'''
 
 @sigopt_cli.command()
 @click.option('--api-token', prompt='SigOpt API token (find at https://app.sigopt.com/tokens/info)')
@@ -15,9 +20,14 @@ Enable log collection'''
   '--enable-log-collection/--no-enable-log-collection',
   prompt=LOG_COLLECTION_PROMPT,
 )
-def config(api_token, enable_log_collection):
+@click.option(
+  '--enable-cell-tracking/--no-enable-cell-tracking',
+  prompt=CELL_TRACKING_PROMPT,
+)
+def config(api_token, enable_log_collection, enable_cell_tracking):
   '''Configure the SigOpt client.'''
   _config.persist_configuration_options({
     _config.API_TOKEN_KEY: api_token,
+    _config.CELL_TRACKING_ENABLED_KEY: enable_cell_tracking,
     _config.LOG_COLLECTION_ENABLED_KEY: enable_log_collection,
   })
