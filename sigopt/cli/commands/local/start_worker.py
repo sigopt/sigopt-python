0b99d741633b7b1451c8e6afda351e68c469ac03
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-07-21 09:09:18 -0700

    fix start_worker

--- sigopt/cli/commands/local/start_worker.py
+++ sigopt/cli/commands/local/start_worker.py
@@ -2,7 +2,7 @@ from sigopt.config import config
 from sigopt.factory import SigOptFactory
 
 from ...utils import cli_experiment_loop
-from ...arguments import experiment_id_argument
+from ...arguments import experiment_id_argument, source_file_option
 from ..base import sigopt_cli
 from ..run_base import run_command
 
@@ -10,8 +10,9 @@ from ..run_base import run_command
 @sigopt_cli.command()
 @experiment_id_argument
 @run_command
-def start_worker(experiment_id, command, run_options):
+@source_file_option
+def start_worker(experiment_id, command, run_options, source_file):
   '''Start a worker for the given Experiment.'''
   factory = SigOptFactory.from_default_project()
   experiment = factory.get_experiment(experiment_id)
-  cli_experiment_loop(config, experiment, command, run_options)
+  cli_experiment_loop(config, experiment, command, run_options, source_file)
