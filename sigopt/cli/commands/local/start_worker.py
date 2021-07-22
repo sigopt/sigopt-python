8dbe55f30d4f9cb3b16616b8dcbea1d32bc416a0
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-07-22 09:07:38 -0700

    catch missing project id

--- sigopt/cli/commands/local/start_worker.py
+++ sigopt/cli/commands/local/start_worker.py
@@ -1,3 +1,5 @@
+import click
+
 from sigopt.config import config
 from sigopt.factory import SigOptFactory
 
@@ -7,12 +9,18 @@ from ..base import sigopt_cli
 from ..run_base import run_command
 
 
-@sigopt_cli.command()
+@sigopt_cli.command(context_settings=dict(
+  allow_interspersed_args=False,
+  ignore_unknown_options=True,
+))
 @experiment_id_argument
 @run_command
 @source_file_option
 def start_worker(experiment_id, command, run_options, source_file):
   '''Start a worker for the given Experiment.'''
   factory = SigOptFactory.from_default_project()
-  experiment = factory.get_experiment(experiment_id)
+  try:
+    experiment = factory.get_experiment(experiment_id)
+  except ValueError as ve:
+    raise click.ClickException(str(ve))
   cli_experiment_loop(config, experiment, command, run_options, source_file)
