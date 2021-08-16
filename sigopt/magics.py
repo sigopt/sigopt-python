e821f51a42df3db8d8dbfd9487584f2e9e34ec0f
Dan W Anderson <dan.w.anderson@intel.com>
2021-08-16 15:33:46 -0400

    one more

--- sigopt/magics.py
+++ sigopt/magics.py
@@ -26,10 +26,10 @@ def get_ns():
   stack_depth = 1
   while True:
     frame = sys._getframe(stack_depth)
-    locals = frame.f_locals
+    f_locals = frame.f_locals
     try:
       if isinstance(locals['exit'], IPython.core.autocall.ExitAutocall):
-        return locals
+        return f_locals
     except KeyError:
       pass
     stack_depth += 1
