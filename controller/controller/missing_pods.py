bb77f6a0930b3ce948d635ef5e4aa26d7ccb86a4
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2021-08-17 14:00:44 -0700

    remaining budget

--- controller/controller/missing_pods.py
+++ controller/controller/missing_pods.py
@@ -2,5 +2,5 @@ def get_missing_experiment_pod_count(conn, current_pod_count, experiment_id):
   e = conn.experiments(experiment_id).fetch()
   expected_num_pods = e.parallel_bandwidth or 1
   if e.budget is not None:
-    expected_num_pods = min(expected_num_pods, e.budget - e.progress.budget_consumed)
+    expected_num_pods = min(expected_num_pods, e.budget - e.progress.remaining_budget)
   return int(max(expected_num_pods - current_pod_count, 0))
