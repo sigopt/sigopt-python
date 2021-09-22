f0fafb9e10579ec5b1a72ab3c5c3ce01479c8165
Dan W Anderson <dan.w.anderson@intel.com>
2021-09-22 14:52:59 -0400

    cleanup

--- sigopt/ratelimit.py
+++ sigopt/ratelimit.py
@@ -7,15 +7,10 @@ global_failed_status_count = 0
 thread_lock = threading.Lock()
 
 class FailedStatusRateLimit(object):
-  def __init__(self, limit, key=None):
+  def __init__(self, limit):
     self.limit = limit
-    self.key = key
 
-  def increment_and_check(self, value=None):
-    if self.key is not None:
-      if value != self.key:
-        self.key = value
-        self.clear()
+  def increment_and_check(self):
     with thread_lock:
       global global_failed_status_count
       global_failed_status_count += 1
