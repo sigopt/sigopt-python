4d286eee453eff32033d7ae4c738c3fa6c97b199
Eric Lee <erichanslee@gmail.com>
2021-10-06 14:47:28 -0700

    add parsing of evals and proper logging of aliases

--- sigopt/xgboost/run.py
+++ sigopt/xgboost/run.py
@@ -1,50 +1,76 @@
 import xgboost
 from xgboost import DMatrix
 import numpy
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, mean_squared_error
 from ..context import Context
 from .. import create_run
+import time
+
+DEFAULT_EVALS_NAME = 'Test Set'
+XGB_ALIASES = \
+  {
+     'learning_rate': 'eta',
+     'min_split_loss': 'gamma',
+     'reg_lambda': 'lambda',
+     'reg_alpha': 'alpha'
+  }
+
+
+def compute_classification_metrics(run, bst, D_matrix):
+  preds = bst.predict(D_matrix)
+  preds = numpy.round(preds)
+  accuracy = accuracy_score(D_matrix.get_labels(), preds)
+  rep = classification_report(D_matrix.get_labels(), preds, output_dict=True, zero_division=0)
+  other_metrics = rep['weighted avg']
+  run.log_metric("accuracy", accuracy)
+  run.log_metric("f1", other_metrics['f1-score'])
+  run.log_metric("recall", other_metrics['recall'])
+  run.log_metric("precision", other_metrics['precision'])
+
+
+def compute_regression_metrics(run, bst, D_test):
+  pass
+
 
 def run(params, D_train, num_boost_round=10, evals=None, run_options=None):
   """
-  Sigopt runs interface
+  Sigopt integration for XGBoost mirrors the standard XGBoost train interface for the most part, with the option
+  for additional arguments. Unlike the usual train interface, run() returns a context object, where context.run
+  and context.model are the resulting run and XGBoost model, respectively.
   """
   assert type(D_train) is DMatrix
+  assert type(evals) is DMatrix or list
 
-  num_eval_sets = 0
-  if evals:
-    num_eval_sets = len(evals) if type(evals) is list else 1
+  # Parse evals argument: if DMatrix argument make instead a list of pairs (and will be None by default)
+  validation_sets = [(evals, DEFAULT_EVALS_NAME)] if type(evals) is DMatrix else evals
 
   run = create_run()
   run.log_model("XGBoost")
-
-  # Log data
   run.log_metadata("Dataset columns", D_train.num_col())
   run.log_metadata("Dataset rows", D_train.num_row())
-
+  run.log_metadata("Objective", params['objective'])
+  if validation_sets:
+    run.log_metadata("Number of Test Sets", len(validation_sets))
 
   # check classification or regression
+  if params['Objective'] is None or params['Objective'].split(':')[0] == 'reg':
+    REGRESSION = True
+  else:
+    CLASSIFICATION = True
 
-  # set params
+  # set and log params, making sure to cross-reference XGB aliases
   for key in params:
-    setattr(run.params, key, params[key])
+    if key in XGB_ALIASES.keys():
+      setattr(run.params, XGB_ALIASES[key], params[key])
+    else:
+      setattr(run.params, key, params[key])
   run.params.num_boost_round = num_boost_round
-  run.log_metadata("Objective", params['objective'])
+
+  # time training
+  t_start = time.time()
   bst = xgboost.train(params, D_train, num_boost_round)
+  t_train = time.time() - t_start
+  run.log_metric("Training time", t_train)
 
-
-
-  # written assuming single Dmatrix
-  # if evals:
-  #   D_test = evals
-  #   preds = bst.predict(D_test)
-  #   preds = numpy.round(preds)
-  #   accuracy = accuracy_score(Y_test, preds)
-  #   rep = classification_report(Y_test, preds, output_dict=True, zero_division=0)
-  #   other_metrics = rep['weighted avg']
-  #   run.log_metric("accuracy", accuracy)
-  #   run.log_metric("f1", other_metrics['f1-score'])
-  #   run.log_metric("recall", other_metrics['recall'])
-  #   run.log_metric("precision", other_metrics['precision'])
 
   return Context(run, bst)
