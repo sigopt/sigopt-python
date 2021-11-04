db1a1ad2a5b65f6cb0f739e06870fa5582b35f79
Eric Lee <erichanslee@gmail.com>
2021-11-04 12:51:20 -0700

    re-add metric unit testing
    move tests around
    
    move some of the integration over
    
    rewrite
    
    tighter
    
    rm test_log_metric
    
    change to task
    
    add reg test against sklearn
    
    accidentally commit swp file
    
    remove redundancy
    
    one for each task

--- integration_test/test_xgboost.py
+++ integration_test/test_xgboost.py
@@ -1,51 +1,121 @@
+import itertools
 import platform
+import pytest
 import random
 
 import sigopt.xgboost
 from sigopt.xgboost.run import PARAMS_LOGGED_AS_METADATA
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+import numpy
 import xgboost as xgb
-import numpy as np
-
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-num_class = 3
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
-D_train = xgb.DMatrix(X_train, label=Y_train)
-D_test = xgb.DMatrix(X_test, label=Y_test)
-
 
 POSSIBLE_PARAMETERS = {
   'eta': 10 ** random.uniform(-4, 1),
   'gamma': random.uniform(0, 4),
   'max_depth': random.randint(1, 5),
   'min_child_weight': random.uniform(0, 3),
-  'num_class': num_class,
   'lambda': random.uniform(1, 3),
   'alpha': 10 ** random.uniform(-4, 0),
-  'objective': random.choice(['multi:softmax', 'multi:softprob']),
   'tree_method': random.choice(['hist', 'exact', 'approx', 'auto']),
-  'eval_metric': ['mlogloss', 'merror'],
 }
 
-def _form_random_run_params():
-  random_subset_size = random.randint(1, len(POSSIBLE_PARAMETERS))
-  subset_keys = random.sample(POSSIBLE_PARAMETERS.keys(), random_subset_size)
-  if 'num_class' not in subset_keys:
-    subset_keys.append('num_class')
-  subset_param = {k: POSSIBLE_PARAMETERS[k] for k in subset_keys}
-  return dict(
-    params=subset_param,
-    dtrain=D_train,
-    evals=[(D_test, 'test0')],
-    num_boost_round=random.randint(3, 15),
-    verbose_eval=False,
-  )
+CLASSIFICATION_METRIC_NAMES = (
+  'accuracy',
+  'F1',
+  'precision',
+  'recall',
+)
+REGRESSION_METRIC_NAMES = (
+  'mean absolute error',
+  'mean squared error',
+)
+
+def _create_random_dataset(task='binary'):
+  if task == 'binary':
+    n_samples = random.randint(180, 300)
+    n_features = random.randint(5, 25)
+    n_classes = 2
+
+    return datasets.make_classification(
+      n_samples=n_samples,
+      n_features=n_features,
+      n_classes=n_classes,
+    )
+  elif task == 'multiclass':
+    n_samples = random.randint(180, 300)
+    n_classes = random.randint(3, 8)
+    n_informative = random.randint(2 * n_classes, 20)
+    n_features = random.randint(n_informative + 2, 40)
+
+    return datasets.make_classification(
+      n_samples=n_samples,
+      n_features=n_features,
+      n_informative=n_informative,
+      n_classes=n_classes,
+    )
+  else:
+    n_samples = random.randint(200, 500)
+    n_features = random.randint(50, 100)
+    n_informative = random.randint(10, n_features - 2)
+
+    return datasets.make_regression(
+      n_samples=n_samples,
+      n_features=n_features,
+      n_informative=n_informative,
+      noise=0.2,
+    )
+
+def _create_random_metric_objective(task='binary'):
+  if task == 'binary':
+    return {
+      'objective': random.choice(['binary:logistic', 'binary:hinge', 'binary:logitraw']),
+      'eval_metric': ['logloss', 'aucpr', 'error'],
+    }
+  elif task == 'multiclass':
+    return {
+      'objective': random.choice(['multi:softmax', 'multi:softprob']),
+      'eval_metric': ['mlogloss', 'merror'],
+    }
+  else:
+    return {
+      'objective': random.choice(['reg:squarederror', 'reg:pseudohubererror']),
+      'eval_metric': ['rmse', 'mae', 'mape'],
+    }
+
 
 class TestXGBoost(object):
-  def _verify_parameter_logging(self, run, params):
+  def _form_random_run_params(self, task):
+    self.task = task
+    self.is_classification = True if self.task in ('binary', 'multiclass') else False
+    X, y = _create_random_dataset(self.task)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+    D_train = xgb.DMatrix(X_train, label=Y_train)
+    D_test = xgb.DMatrix(X_test, label=Y_test)
+
+    possible_params = POSSIBLE_PARAMETERS
+    random_subset_size = random.randint(1, len(possible_params))
+    subset_keys = random.sample(possible_params.keys(), random_subset_size)
+    subset_params = {k: possible_params[k] for k in subset_keys}
+    subset_params.update(_create_random_metric_objective(self.task))
+    if self.task == 'multiclass':
+      subset_params.update({'num_class': len(numpy.unique(y))})
+
+    run_options = {
+      'name': 'dev-integration-test'
+    }
+
+    self.run_params = dict(
+      params=subset_params,
+      dtrain=D_train,
+      evals=[(D_test, 'test0')],
+      num_boost_round=random.randint(3, 15),
+      verbose_eval=False,
+      run_options=run_options,
+    )
+
+  def _verify_parameter_logging(self, run):
+    params = self.run_params['params']
     for p in params.keys():
       if p not in PARAMS_LOGGED_AS_METADATA:
         assert params[p] == run.assignments[p]
@@ -54,21 +124,43 @@ class TestXGBoost(object):
           assert params[p] == run.metadata[p]
         else:
           assert str(params[p]) == run.metadata[p]
+    assert run.assignments['num_boost_round'] == self.run_params['num_boost_round']
 
-  def _verify_metric_logging(self, run, params):
-    pass
+  def _verify_metric_logging(self, run):
+    data_names = ['Training Set']
+    if self.run_params['evals']:
+      data_names.extend([e[-1] for e in self.run_params['evals']])
+    if self.is_classification:
+      for d_name, m_name in itertools.product(data_names, CLASSIFICATION_METRIC_NAMES):
+        assert 0 <= run.values['-'.join((d_name, m_name))].value <= 1
+    else:
+      for d_name, m_name in itertools.product(data_names, REGRESSION_METRIC_NAMES):
+        assert run.values['-'.join((d_name, m_name))].value >= 0
 
-  def test_run(self):
-    xgb_params = _form_random_run_params()
-    ctx = sigopt.xgboost.run(**xgb_params)
-    run = sigopt.get_run(ctx.run.id)
-    assert run.metadata['Dataset columns'] == 4
-    assert run.metadata['Dataset rows'] == 120
-    assert run.metadata['Number of Test Sets'] == 1
+    if self.run_params['params']['eval_metric']:
+      for d_name, m_name in itertools.product(data_names[1:], self.run_params['params']['eval_metric']):
+        assert run.values['-'.join((d_name, m_name))]
+
+    assert run.values['Training time'].value > 0
+
+
+  def _verify_metadata_logging(self, run):
+    assert run.metadata['Dataset columns'] == self.run_params['dtrain'].num_col()
+    assert run.metadata['Dataset rows'] == self.run_params['dtrain'].num_row()
+    assert run.metadata['Number of Test Sets'] == len(self.run_params['evals'])
     assert run.metadata['Python Version'] == platform.python_version()
     assert run.metadata['XGBoost Version'] == xgb.__version__
-    self._verify_parameter_logging(run, xgb_params['params'])
-    assert run.assignments['num_boost_round'] == xgb_params['num_boost_round']
+
+  @pytest.mark.parametrize("task", ['binary', 'multiclass', 'regression'])
+  def test_run(self, task):
+    self._form_random_run_params(task)
+    ctx = sigopt.xgboost.run(**self.run_params)
+    run = sigopt.get_run(ctx.run.id)
+    if 'name' in self.run_params['run_options']:
+      assert run.name == self.run_params['run_options']['name']
+    self._verify_metadata_logging(run)
+    self._verify_parameter_logging(run)
+    self._verify_metric_logging(run)
 
     feature_importances = run.sys_metadata['feature_importances']
     real_scores = sorted(ctx.model.get_score(importance_type='weight').items(), key=lambda x:(x[1], x[0]), reverse=True)
@@ -77,4 +169,7 @@ class TestXGBoost(object):
     assert saved_scores and len(saved_scores) <= len(real_scores)
     real_scores = real_scores[:len(saved_scores)]
     assert [k for k, v in real_scores] == [k for k, v in saved_scores]
-    assert np.allclose(np.array([v for k, v in real_scores]), np.array([v for k, v in saved_scores]))
+    assert numpy.allclose(
+      numpy.array([v for k, v in real_scores]),
+      numpy.array([v for k, v in saved_scores]),
+    )
---
+++ test/xgboost/test_compute_metric.py
@@ -0,0 +1,91 @@
+import numpy
+from sigopt.xgboost.compute_metrics import (
+  compute_classification_report,
+  compute_mae,
+  compute_mse,
+  compute_accuracy
+)
+from sklearn.metrics import (
+  accuracy_score,
+  classification_report,
+  mean_absolute_error,
+  mean_squared_error
+)
+
+def verify_classification_metrics_against_sklearn(y_true, y_pred):
+  report_compute = compute_classification_report(y_true, y_pred)
+  report_sklearn = classification_report(y_true, y_pred, output_dict=True, zero_division=0)
+  assert numpy.isclose(report_compute['weighted avg']['precision'], report_sklearn['weighted avg']['precision'])
+  assert numpy.isclose(report_compute['weighted avg']['recall'], report_sklearn['weighted avg']['recall'])
+  assert numpy.isclose(report_compute['weighted avg']['f1-score'], report_sklearn['weighted avg']['f1-score'])
+  assert numpy.abs(compute_accuracy(y_true, y_pred) - accuracy_score(y_true, y_pred)) < 1e-8
+  classes = numpy.unique(y_true)
+  for c in classes:
+    label = str(c)
+    assert numpy.isclose(report_compute[label]['precision'], report_sklearn[label]['precision'])
+    assert numpy.isclose(report_compute[label]['recall'], report_sklearn[label]['recall'])
+    assert numpy.isclose(report_compute[label]['f1-score'], report_sklearn[label]['f1-score'])
+
+class TestComputeMetrics(object):
+  def test_binary_classification_one_true_label(self):
+    y_true = numpy.zeros(10, dtype=int)
+    y_pred = numpy.ones(10, dtype=int)
+    assert compute_accuracy(y_true, y_pred) == 0
+    report = compute_classification_report(y_true, y_pred)
+    assert '1' not in report.keys()
+    assert report['0']['precision'] == 0
+    assert report['0']['recall'] == 0
+    assert report['0']['f1-score'] == 0
+    assert report['weighted avg']['precision'] == 0
+    assert report['weighted avg']['recall'] == 0
+    assert report['weighted avg']['f1-score'] == 0
+
+  def test_binary_classification_one_pred_label(self):
+    y_true = numpy.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0])
+    y_pred = numpy.ones(10, dtype=int)
+    assert compute_accuracy(y_true, y_pred) == 0.3
+    report = compute_classification_report(y_true, y_pred)
+    assert report['0']['precision'] == 0
+    assert report['0']['recall'] == 0
+    assert report['0']['f1-score'] == 0
+    assert report['1']['precision'] == 0.3
+    assert report['1']['recall'] == 1.0
+    assert numpy.isclose(report['1']['f1-score'], 2 * (1.0 * 0.3) / (1.0 + 0.3))
+    assert report['weighted avg']['precision'] == 0.3 * 0.3
+    assert report['weighted avg']['recall'] == 0.3
+    assert numpy.isclose(report['weighted avg']['f1-score'], 2 * (1.0 * 0.3) / (1.0 + 0.3) * 0.3)
+
+  def test_binary_classification_against_sklearn(self):
+    n_samples = 30
+    y_true = numpy.random.randint(2, size=n_samples)
+    y_pred = numpy.random.randint(2, size=n_samples)
+    verify_classification_metrics_against_sklearn(y_true, y_pred)
+
+  def test_multiclass_classification_metrics(self):
+    n_samples = 50
+    y_true = numpy.random.randint(3, size=n_samples)
+    y_pred = numpy.random.randint(2, size=n_samples)
+    verify_classification_metrics_against_sklearn(y_true, y_pred)
+
+    y_pred = numpy.random.randint(3, size=n_samples)
+    verify_classification_metrics_against_sklearn(y_true, y_pred)
+
+  def test_regression_metrics(self):
+    # Check regression metrics
+    n_samples = 10
+    y_true = 2 * numpy.ones(n_samples)
+    assert numpy.isclose(compute_mae(y_true, numpy.zeros(n_samples)), 2.0)
+    assert numpy.isclose(compute_mse(y_true, numpy.zeros(n_samples)), 4.0)
+
+    y_true = numpy.random.randn(n_samples)
+    assert numpy.isclose(compute_mae(y_true, y_true), 0)
+    assert numpy.isclose(compute_mse(y_true, y_true), 0)
+
+    y_pred = numpy.random.randn(n_samples)
+    assert numpy.isclose(compute_mae(y_true, y_pred), compute_mae(y_pred, y_true))
+    assert numpy.isclose(compute_mse(y_true, y_pred), compute_mse(y_pred, y_true))
+    assert numpy.isclose(compute_mae(y_true, y_pred), numpy.sum(numpy.abs(y_true - y_pred)) / n_samples)
+    assert numpy.isclose(compute_mse(y_true, y_pred), numpy.sum((y_true - y_pred) ** 2) / n_samples)
+
+    assert numpy.isclose(compute_mae(y_true, y_pred), mean_absolute_error(y_true, y_pred))
+    assert numpy.isclose(compute_mse(y_true, y_pred), mean_squared_error(y_true, y_pred))
