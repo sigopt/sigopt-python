0e6506d3d3fddf53502b6581585fc15318d5397e
Eric Hans Lee <erichanslee@gmail.com>
2021-11-04 10:03:28 -0700

    added manual compute metrics, requires numpy still (#281)
    * added manual compute metrics, requires numpy still
    
    * fix always false flag
    
    * fix lint
    
    * add unit test
    
    * clean up unit test
    
    * clean up unit tests, remove sklearn stuff altogether
    
    * test compute metrics done
    
    * add metric logging and computation unit test
    
    * separate tests into computing and logging metrics
    
    * remove unused imports
    
    * consolidate tests
    
    * re-add sklearn, whoops
    
    * remove integration tests
    
    * fix sklearn patch typo

--- sigopt/xgboost/compute_metrics.py
+++ sigopt/xgboost/compute_metrics.py
@@ -1,10 +1,57 @@
 import numpy
-from sklearn.metrics import (
-  accuracy_score,
-  classification_report,
-  mean_absolute_error,
-  mean_squared_error
-)
+
+
+def compute_positives_and_negatives(y_true, y_pred, class_label):
+  y_true_equals = y_true == class_label
+  y_true_notequals = y_true != class_label
+  y_pred_equals = y_pred == class_label
+  y_pred_notequals = y_pred != class_label
+  tp = numpy.count_nonzero(numpy.logical_and(y_true_equals, y_pred_equals))
+  tn = numpy.count_nonzero(numpy.logical_and(y_true_notequals, y_pred_notequals))
+  fp = numpy.count_nonzero(numpy.logical_and(y_true_notequals, y_pred_equals))
+  fn = numpy.count_nonzero(numpy.logical_and(y_true_equals, y_pred_notequals))
+  return tp, tn, fp, fn
+
+
+def compute_accuracy(y_true, y_pred):
+  accuracy = numpy.count_nonzero(y_true == y_pred) / len(y_true)
+  return accuracy
+
+
+def compute_classification_report(y_true, y_pred):
+  classes = numpy.unique(y_true)
+  classification_report = {}
+  classification_report['weighted avg'] = {
+    'f1-score': 0,
+    'recall': 0,
+    'precision': 0
+  }
+  for class_label in classes:
+    tp, _, fp, fn = compute_positives_and_negatives(y_true, y_pred, class_label)
+    precision = tp / (tp + fp) if (tp + fp)!=0 else 0
+    recall = tp / (tp + fn) if (tp + fn)!=0 else 0
+    f1 = tp / (tp + 0.5 * (fp + fn)) if (tp + 0.5 * (fp + fn))!=0 else 0
+    support = numpy.count_nonzero(y_true == class_label)
+    classification_report[str(class_label)] = {
+      'precision': precision,
+      'recall': recall,
+      'f1-score': f1,
+      'support': support
+    }
+    classification_report['weighted avg']['precision'] += (support / len(y_pred)) * precision
+    classification_report['weighted avg']['recall'] += (support / len(y_pred)) * recall
+    classification_report['weighted avg']['f1-score'] += (support / len(y_pred)) * f1
+  return classification_report
+
+
+def compute_mae(y_true, y_pred):
+  d = y_true - y_pred
+  return numpy.mean(abs(d))
+
+
+def compute_mse(y_true, y_pred):
+  d = y_true - y_pred
+  return numpy.mean(d ** 2)
 
 
 def compute_classification_metrics(run, bst, D_matrix_pair):
@@ -12,8 +59,8 @@ def compute_classification_metrics(run, bst, D_matrix_pair):
   preds = bst.predict(D_matrix)
   preds = numpy.round(preds)
   y_test = D_matrix.get_label()
-  accuracy = accuracy_score(y_test, preds)
-  rep = classification_report(y_test, preds, output_dict=True, zero_division=0)
+  accuracy = compute_accuracy(y_test, preds)
+  rep = compute_classification_report(y_test, preds)
   other_metrics = rep['weighted avg']
   classification_metrics = {
     f"{D_name}-accuracy" : accuracy,
@@ -30,7 +77,7 @@ def compute_regression_metrics(run, bst, D_matrix_pair):
   preds = numpy.round(preds)
   y_test = D_matrix.get_label()
   regression_metrics = {
-    f"{D_name}-mean absolute error": mean_absolute_error(y_test, preds),
-    f"{D_name}-mean squared error": mean_squared_error(y_test, preds)
+    f"{D_name}-mean absolute error": compute_mae(y_test, preds),
+    f"{D_name}-mean squared error": compute_mse(y_test, preds)
   }
   run.log_metrics(regression_metrics)
