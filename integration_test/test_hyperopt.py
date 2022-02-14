9e3501dc0cdfb687b016b08b23d5c11e6922fc43
yuhong-sigopt <89940019+yuhong-sigopt@users.noreply.github.com>
2022-02-14 09:21:59 -0800

    Add Hyperopt integration (#324)

---
+++ integration_test/test_hyperopt.py
@@ -0,0 +1,118 @@
+from hyperopt import fmin, tpe, hp, STATUS_OK, STATUS_FAIL
+import hyperopt
+from sigopt.hyperopt import SigOptTrials, upload_trials
+import sigopt
+import numpy as np
+import pytest
+import time
+
+def objective(p, threshold=1.0, max_sleep_time=0.0):
+  x, y = p['x'], p['y']
+  if max_sleep_time > 0:
+    time.sleep(np.random.rand() * max_sleep_time)
+  w = np.random.rand()
+  if w <= threshold:
+    return {
+      'loss': x ** 2 + y ** 2,
+      'sum': x + y,
+      'status': STATUS_OK
+    }
+  else:
+    return {
+      'status': STATUS_FAIL
+    }
+
+objective_random = lambda x: objective(x, 0.8)
+objective_success = objective
+objective_fail = lambda x: objective(x, -1.0)
+
+class TestHyperopt(object):
+  def run_fmin(self, online=True, upload=True, objective=objective_success, max_evals=3, **kwargs):
+    project = 'hyperopt-integration-test'
+    trials = SigOptTrials(project=project, online=(online and upload))
+    try:
+      best = fmin(objective,
+                  space={
+                    'x' : hp.uniform('x', -10, 10),
+                    'y': hp.uniform('y', -10, 10)
+                  },
+                  algo=tpe.suggest,
+                  max_evals=max_evals,
+                  trials=trials,
+                  **kwargs
+                  )
+    except hyperopt.exceptions.AllTrialsFailed:
+      best = None
+    if upload and not online:
+      trials.upload()
+    return trials, best
+
+  def test_upload_trials(self):
+    trials, _ = self.run_fmin(upload=False)
+    tids = upload_trials('hyperopt-integration-test', trials)
+    self._verify_uploaded_trials(trials, tids)
+
+  def test_trials_delete_all(self):
+    trials, _ = self.run_fmin(upload=True)
+    assert(len(trials) > 0 and len(trials) == len(trials.uploaded_tids))
+    trials.delete_all()
+    assert(len(trials) == 0 and len(trials.uploaded_tids) == 0)
+
+  @pytest.mark.parametrize("online, upload",
+                           [(False, True),
+                            (True, True),
+                            (False, False),
+                            ])
+  @pytest.mark.parametrize("objective",
+                           [objective_random,
+                            objective_success,
+                            objective_fail])
+  def test_fmin(self, online, upload, objective, max_evals=3, **kwargs):
+    trials, best = self.run_fmin(online, upload, objective, max_evals, **kwargs)
+    self._verify_best_trial(best, trials, objective)
+    self._verify_runs(trials, upload, max_evals)
+
+  def _verify_best_trial(self, best, trials, objective):
+    if objective is objective_fail:
+      assert best is None
+    if objective is objective_success:
+      assert best is not None
+    losses = [r['loss'] for r in trials.results if r['status'] == STATUS_OK]
+    assert ((not best) and (not losses)) or (best and losses)
+    if best:
+      loss = objective_success(best)['loss']
+      assert loss == min(losses)
+
+  def _verify_runs(self, trials, upload, max_evals):
+    if not upload:
+      assert len(trials.uploaded_tids) == 0
+      return
+    assert len(trials.uploaded_tids) == max_evals
+    self._verify_uploaded_trials(trials, trials.uploaded_tids)
+
+  def _verify_uploaded_trials(self, trials, uploaded_tids):
+    trial_dict = {tid: (result, parameters)
+                  for tid, result, parameters in zip(trials.tids, trials.results,
+                                                     trials.parameters)}
+
+    def run_result(run):
+      result = {}
+      if run.state == "completed":
+        result['status'] = STATUS_OK
+        for m in run.values:
+          result[m] = run.values[m].value
+      elif run.state == "failed":
+        result['status'] = STATUS_FAIL
+      else:
+        result['status'] = None
+      return result
+
+    def run_parameters(run):
+      return dict(run.assignments)
+
+    run_dict = {}
+    for tid, run_id in uploaded_tids.items():
+      run = sigopt.get_run(run_id)
+      run_dict[tid] = (run_result(run), run_parameters(run))
+
+    assert trial_dict == run_dict
---
+++ sigopt/hyperopt/base.py
@@ -0,0 +1,114 @@
+from .compat import Trials, STATUS_OK, STATUS_FAIL
+from ..local_run_context import LocalRunContext
+from .. import SigOptFactory
+from ..defaults import get_default_name
+
+
+HYPEROPT_SOURCE_NAME = 'Hyperopt Suggest'
+HYPEROPT_SOURCE_PRIORITY = 1
+
+
+class SigOptTrials(object):
+  def __init__(self, project, trials=None, online=True):
+    self.factory = SigOptFactory(project)
+    self.online = online
+    self._trials = trials if trials else Trials()
+    self.uploaded_tids = {}
+
+    if online:
+      self.saved_refresh = getattr(self._trials, 'refresh')
+      def new_refresh():
+        r = self.saved_refresh()
+        self.do_refresh()
+        return r
+      setattr(self._trials, 'refresh', new_refresh)
+
+  @property
+  def parameters(self):
+    return [self.trial_parameters(trial) for trial in self.trials]
+
+  def upload(self, trials=None, validate=False):
+    new_trials = []
+    trials = trials if trials is not None else self.trials
+    if validate:
+      for trial in trials:
+        self._trials.assert_valid_trial(trial)
+    for trial in trials:
+      result = trial['result']
+      status = result.get('status')
+      if status in [STATUS_OK, STATUS_FAIL]:
+        tid = trial['tid']
+        if tid not in self.uploaded_tids:
+          new_trials.append(trial)
+    ids = self._upload(new_trials)
+    self.uploaded_tids.update(ids)
+    return ids
+
+  def trial_to_run(self, trial):
+    metadata = {'optimizer': 'hyperopt'}
+    result = trial['result']
+    metrics = {k:v for k, v in result.items() if isinstance(v, (int, float))}
+    parameters = self.trial_parameters(trial)
+    status = result.get('status')
+    run = LocalRunContext(name=get_default_name(self.factory.project), metadata=metadata)
+    run.log_parameters(
+      parameters,
+      source=HYPEROPT_SOURCE_NAME,
+      source_meta={
+        'sort': HYPEROPT_SOURCE_PRIORITY,
+        'default_show': True
+      })
+    if status == STATUS_OK:
+      if not metrics:
+        raise ValueError('No metrics found in trial result')
+      run.log_metrics(metrics)
+      run.log_state('completed')
+    elif status == STATUS_FAIL:
+      run.log_failure()
+    else:
+      raise ValueError(f'status must be {STATUS_OK} or {STATUS_FAIL}, actually {status}')
+    return run.get()
+
+  def _upload(self, trials):
+    runs = [self.trial_to_run(trial) for trial in trials]
+    runs = self.factory.upload_runs(runs)
+    return {trial['tid']:run.id for trial, run in zip(trials, runs)}
+
+  def trial_parameters(self, trial):
+    vals = trial.get('misc', {}).get('vals', {})
+    rval = {}
+    for k, v in vals.items():
+      if v:
+        rval[k] = v[0]
+    return rval
+
+  def do_refresh(self):
+    if self.online:
+      self.upload()
+
+  def delete_all(self):
+    self.uploaded_tids.clear()
+    self._trials.delete_all()
+
+  def __iter__(self):
+    return iter(self._trials)
+
+  def __len__(self):
+    return len(self._trials)
+
+  def __getitem__(self, item):
+    return self._trials[item]
+
+  def __getattr__(self, name):
+    attrs = dir(self)
+    if name in attrs:
+      return attrs[name]
+    try:
+      return getattr(self._trials, name)
+    except AttributeError as e:
+      raise AttributeError(f"{self.__class__.__name__} object has no attribute {name}") from e
+
+
+def upload_trials(project, trials):
+  st = SigOptTrials(project=project, online=False)
+  return st.upload(trials, validate=True)
