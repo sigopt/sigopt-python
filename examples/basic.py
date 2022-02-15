55c4987bd6f051fb599f11224b94da7a057d70e3
Christopher Manteuffel <christopher.manteuffel@intel.com>
2022-02-15 10:18:02 -0700

    Clean up

--- examples/basic.py
+++ examples/basic.py
@@ -4,9 +4,9 @@ import sigopt
 
 # Take a suggestion from sigopt and evaluate your function
 def execute_model(run):
-  #train a model
-  #evaluate a model
-  #return the accuracy 
+  # train a model
+  # evaluate a model
+  # return the accuracy 
   raise NotImplementedError("Return a number, which represents your metric for this run")
 
 if __name__ == '__main__':
@@ -15,18 +15,18 @@ if __name__ == '__main__':
   parser.add_argument('--client_token', required=True, help="Find your CLIENT_TOKEN at https://sigopt.com/tokens")
   the_args = parser.parse_args()
 
-  connection = Connection(client_token=the_args.client_token, _show_deprecation_warning=False)
-
-  
-  sigopt.log_dataset("Example dataset") #Descriptor of what kind of dataset you are modeling
-  sigopt.log_metadata(key="Dataset Source", value="Example Source") #Useful for keeping track of where you got the data 
-  sigopt.log_metadata(key="Feature Pipeline Name", value="Example Pipeline") #e.g. Sklern, xgboost, etc.
-  sigopt.log_model("Example Model Technique") # What kind of learning you are attemping
+  # Descriptor of what kind of dataset you are modeling
+  sigopt.log_dataset("Example dataset") 
+  # Useful for keeping track of where you got the data
+  sigopt.log_metadata(key="Dataset Source", value="Example Source")  
+  # e.g. Sklern, xgboost, etc.
+  sigopt.log_metadata(key="Feature Pipeline Name", value="Example Pipeline") 
+  # What kind of learning you are attemping
+  sigopt.log_model("Example Model Technique") 
   # Create an experiment with one paramter, x
   experiment = sigopt.create_experiment(
     name="Basic Test experiment",
     project="sigopt-examples",
-    type="offline",
     parameters=[{'name': 'x', 'bounds': {'max': 50.0, 'min': 0.0}, 'type': 'double'}],
     metrics=[{"name":"holdout_accuracy", "objective":"maximize"}],
     parallel_bandwidth=1,
--- examples/parallel.py
+++ examples/parallel.py
@@ -2,26 +2,26 @@ import argparse
 
 import sigopt
 
-# Take a suggestion from sigopt and evaluate your function
-def execute_model(run):
-  #train a model
-  #evaluate a model
-  #return the accuracy 
-  raise NotImplementedError("Return a number, which represents your metric for this run")
+def run_command_on_machine(machine_number, command):
+  # log into machine
+  # execute command
+  raise NotImplementedError("Log into the specified machines, execute the included command")
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--observation_budget', type=int, default=20)
   parser.add_argument('--client_token', required=True, help="Find your CLIENT_TOKEN at https://sigopt.com/tokens")
   the_args = parser.parse_args()
-
-  connection = Connection(client_token=the_args.client_token, _show_deprecation_warning=False)
-
   
-  sigopt.log_dataset("Example dataset") #Descriptor of what kind of dataset you are modeling
-  sigopt.log_metadata(key="Dataset Source", value="Example Source") #Useful for keeping track of where you got the data 
-  sigopt.log_metadata(key="Feature Pipeline Name", value="Example Pipeline") #e.g. Sklern, xgboost, etc.
-  sigopt.log_model("Example Model Technique") # What kind of learning you are attemping
+  # Descriptor of what kind of dataset you are modeling
+  sigopt.log_dataset("Example dataset") 
+  # Useful for keeping track of where you got the data 
+  sigopt.log_metadata(key="Dataset Source", value="Example Source") 
+  # e.g. Sklern, xgboost, etc.
+  sigopt.log_metadata(key="Feature Pipeline Name", value="Example Pipeline") 
+  # What kind of learning you are attemping
+  sigopt.log_model("Example Model Technique") 
   # Create an experiment with one paramter, x
   experiment = sigopt.create_experiment(
     name="Basic Test experiment",
@@ -29,12 +29,13 @@ if __name__ == '__main__':
     type="offline",
     parameters=[{'name': 'x', 'bounds': {'max': 50.0, 'min': 0.0}, 'type': 'double'}],
     metrics=[{"name":"holdout_accuracy", "objective":"maximize"}],
-    parallel_bandwidth=3, #number of machines you are running learning on 
+    parallel_bandwidth=3, # number of machines you are running learning on 
     observation_budget=the_args.observation_budget,
   )
   print('Created experiment id {0}'.format(experiment.id))
 
-  # In a loop: receive a suggestion, evaluate the metric, report an observation
+  # In a loop: on each machine, start off a learning process, then each reports separately 
+  # to Sigopt the results
  for machine_number in range(experiment.parallel_bandwidth):
   run_command_on_machine(
     machine_number,
