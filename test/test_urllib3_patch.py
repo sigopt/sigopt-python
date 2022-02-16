12bb237eeb417160f504cf4c969143324a1dfbeb
Taylor Jackle Spriggs <74561858+tjs-intel@users.noreply.github.com>
2022-02-16 12:24:11 -0500

    Revive keepalive (#333)
    * patch urllib3 classes
    
    * update requestor to use modified session
    
    * fix typo
    
    * test expiration
    
    * spacing
    
    * mount once
    
    * dont assert no session

---
+++ test/test_urllib3_patch.py
@@ -0,0 +1,19 @@
+import pytest
+
+from sigopt.urllib3_patch import ExpiringHTTPConnectionPool, ExpiringHTTPSConnectionPool
+
+@pytest.mark.parametrize("pool_cls", [ExpiringHTTPConnectionPool, ExpiringHTTPSConnectionPool])
+def test_pool_reuses_connections(pool_cls):
+  pool = pool_cls(host="sigopt.com", expiration_seconds=30)
+  conn1 = pool._get_conn()
+  pool._put_conn(conn1)
+  conn2 = pool._get_conn()
+  assert conn1 is conn2
+
+@pytest.mark.parametrize("pool_cls", [ExpiringHTTPConnectionPool, ExpiringHTTPSConnectionPool])
+def test_pool_expires_connections(pool_cls):
+  pool = pool_cls(host="sigopt.com", expiration_seconds=0)
+  conn1 = pool._get_conn()
+  pool._put_conn(conn1)
+  conn2 = pool._get_conn()
+  assert conn1 is not conn2
