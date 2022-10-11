af45be86027cebd40b25ab596b00d5a5f499ced7
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2022-10-11 13:53:29 -0400

    better error handling

--- sigopt/orchestrate/cluster/context.py
+++ sigopt/orchestrate/cluster/context.py
@@ -1,4 +1,4 @@
-from .errors import PleaseDisconnectError
+from .errors import NotConnectedError
 
 
 class DisconnectOnException(object):
@@ -13,7 +13,7 @@ class DisconnectOnException(object):
     if exc is not None:
       try:
         self._services.cluster_service.disconnect(cluster_name=self._cluster_name, disconnect_all=False)
-        return False
-      except Exception as disconnect_exception:
-        raise PleaseDisconnectError(self._cluster_name) from disconnect_exception
+      except NotConnectedError:
+        pass
+      return False
     return None
