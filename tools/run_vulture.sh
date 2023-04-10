ce1f1f42b3ddd559309b107f7a68d60caa287cc4
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-10 14:31:17 -0600

    fix custom shellcheck

--- tools/run_vulture.sh
+++ tools/run_vulture.sh
@@ -2,5 +2,7 @@
 # Copyright © 2023 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
+set -e
+set -o pipefail
 
 exec vulture --exclude="build,venv" --ignore-decorators="@click.*,@sigopt_cli.*,@pytest.*" "$@"
