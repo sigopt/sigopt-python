c9b58453af68cc8ce52ddcbdb54b14b8021c3ba7
Christopher Manteuffel <christopher.manteuffel@intel.com>
2024-04-03 12:57:14 -0700

    Fix for dict_keys

--- test/xgboost/test_run_options_parsing.py
+++ test/xgboost/test_run_options_parsing.py
@@ -115,7 +115,7 @@ class TestRunOptionsParsing(object):
 
   def test_run_options_fully_parsed(self):
     num_of_options = random.randint(1, len(DEFAULT_RUN_OPTIONS))
-    run_options_keys = random.sample(DEFAULT_RUN_OPTIONS.keys(), num_of_options)
+    run_options_keys = random.sample(sorted(DEFAULT_RUN_OPTIONS.keys()), num_of_options)
     run_options = {k: DEFAULT_RUN_OPTIONS[k] for k in run_options_keys}
     parsed_options = parse_run_options(run_options)
     assert set(parsed_options.keys()) == set(DEFAULT_RUN_OPTIONS.keys())
